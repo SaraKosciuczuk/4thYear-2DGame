e29f0dff84cb92d98d9c7de9279f5e4996eac592
Sara <C00251415@itcarlow.ie>
2023-01-23 15:34:18 +0000

    switched to rectangle tilemap, added player controller

--- Isometric_Game/Assets/PlayerMovement.cs
+++ Isometric_Game/Assets/PlayerMovement.cs
@@ -8,13 +8,12 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
 
     Vector2 movement;
-    
+
     // Update is called once per frame
     void Update()
     {
-        // Input
         movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        //movement.y = Input.GetAxisRaw("Vertical");
     }
 
     void FixedUpdate()
