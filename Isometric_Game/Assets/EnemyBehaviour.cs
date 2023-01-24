f572463df6db9ca0c53cc2d7fbb59a60454fb73a
Sara <C00251415@itcarlow.ie>
2023-01-24 10:35:12 +0000

    new movement, created ai for enemy

---
+++ Isometric_Game/Assets/EnemyBehaviour.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+
+    Rigidbody2D enemyBody;
+
+    //bool isFacingRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFacingRight())
+        {
+            enemyBody.velocity = new Vector2(moveSpeed, 0f);
+        }else
+        {
+            enemyBody.velocity = new Vector2(-moveSpeed, 0f);
+        }
+    }
+
+    private bool isFacingRight()
+    {
+        return transform.localScale.x > Mathf.Epsilon;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(enemyBody.velocity.x)), transform.localScale.y);
+    }
+}
--- Isometric_Game/Assets/PlayerMovement.cs
+++ Isometric_Game/Assets/PlayerMovement.cs
@@ -4,21 +4,37 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public Rigidbody2D rb;
+    //public float moveSpeed = 5f;
+    //public Rigidbody2D rb;
+    //Vector2 movement;
 
-    Vector2 movement;
+    public PlayerController controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMovement = 0f;
+    bool jump = false;
 
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
+        //movement.x = Input.GetAxisRaw("Horizontal");
         //movement.y = Input.GetAxisRaw("Vertical");
+
+        horizontalMovement = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
     }
 
     void FixedUpdate()
     {
         // Movement
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        //rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        controller.Move(horizontalMovement * Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
 }
